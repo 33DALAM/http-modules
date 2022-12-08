e0f5c01a160e387b07a773a637ddf1221eb115d1
33DALAM <naveeedalam52@gmail.com>
2022-12-08 16:03:47 +0000

    thanks

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-console .log ("safe city office")
-
-
-
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('heloo world!');
+  res.end('heloo my name is naveed!');
 }).listen(6060); 
+console console.log(hloo);
